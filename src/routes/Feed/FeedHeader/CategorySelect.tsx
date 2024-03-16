034bc802cfba59832b6f71db3f706408359b8299
Chaitanya Swaroop Kumar Allu <chaitanyaa1999@gmail.com>
2024-03-16 00:02:24 -0500

    corrected category select err

--- src/routes/Feed/FeedHeader/CategorySelect.tsx
+++ src/routes/Feed/FeedHeader/CategorySelect.tsx
@@ -26,7 +26,7 @@ const CategorySelect: React.FC<Props> = () => {
   return (
     <StyledWrapper>
       <div ref={dropdownRef} className="wrapper" onClick={handleOpen}>
-        {currentCategory} <MdExpandMore />
+        {currentCategory} s <MdExpandMore />
       </div>
       {opened && (
         <div className="content">
