5ab5efd8877bb6120facbb7e271a3f746faa79b4
Chaitanya Swaroop Kumar Allu <chaitanyaa1999@gmail.com>
2024-03-15 19:55:49 -0500

    bug fix

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -5,7 +5,7 @@ import styled from "@emotion/styled"
 const Logo = () => {
   return (
     <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
-      <strong>{CONFIG.blog.title} 🧑‍💻</strong>
+      {CONFIG.blog.title} 🧑‍💻
     </StyledWrapper>
   )
 }
